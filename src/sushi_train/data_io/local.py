d122b0cde94f89762d13ac4469c7d3fe0ad6a802
Michael Galo <michael@michaelthinks.co>
2025-08-31 11:56:10 -0500

    added error printing for local.py

--- src/sushi_train/data_io/local.py
+++ src/sushi_train/data_io/local.py
@@ -4,40 +4,46 @@ def write_dataframe_to_local_csv(dataframe, file_path):
     try:
         result = dataframe.to_csv(file_path, index=False)
         return result
-    except Exception:
+    except Exception as e:
+        print(f"Error in writing dataframe locally: {e}")
         return None
 
 def read_local_csv_to_dataframe(file_path):
     try:
         result = pl.read_csv(file_path)
         return result
-    except Exception:
+    except Exception as e:
+        print(f"Error in reading local file: {e}")
         return None
     
 def write_dataframe_to_local_parquet(dataframe, file_path):
     try:
         result = dataframe.to_parquet(file_path, index=False)
         return result
-    except Exception:
+    except Exception as e:
+        print(f"Error in writing dataframe locally: {e}")
         return None
     
 def read_local_parquet_to_dataframe(file_path):
     try:
         result = pl.read_parquet(file_path)
         return result
-    except Exception:
+    except Exception as e:
+        print(f"Error in reading local file: {e}")
         return None
 
 def write_dataframe_to_local_json(dataframe, file_path):
     try:
         result = dataframe.to_json(file_path, index=False)
         return result
-    except Exception:
+    except Exception as e:
+        print(f"Error in writing dataframe locally: {e}")
         return None
 
 def read_local_json_to_dataframe(file_path):
     try:
         result = pl.read_json(file_path)
         return result
-    except Exception:
+    except Exception as e:
+        print(f"Error in reading local file: {e}")
         return None
